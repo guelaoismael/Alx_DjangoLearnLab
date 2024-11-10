c1a506a4fb2948857c8fe815f01b99f0575e1ed1
guelaoismael <guelaoismael@gmail.com>
2024-11-10 10:46:51 +0100

    add new app relationship_app

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -14,4 +14,4 @@ class Library(models.Model):
 
 class Librarian(models.Model):
   name = models.CharField(max_length=120)
-  library = models.OneToOneField(Library)
+  library = models.OneToOneField(Library, on_delete= models.CASCADE)
