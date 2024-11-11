c9022926e02a7c045bd93978f2c4d6361fdc80fc
guelaoismael <guelaoismael@gmail.com>
2024-11-11 18:08:55 +0100

    add new app relationship_app

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,6 +3,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 def get_books_by_author(author_name):
    
         author = Author.objects.get(name=author_name)
+
         books_by_author = Book.objects.filter(author=author)
         return books_by_author
 
@@ -13,5 +14,5 @@ def get_books_in_library(library_name):
    
 
 def get_librarian_for_library(library_name):
-        library = Library.objects.get(name=library_name)
-        return library.librarian
+        librarian = Librarian.objects.get(library=library_name)
+        return librarian
