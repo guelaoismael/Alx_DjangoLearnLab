40900b5c525528bcc740288410ef76610d532690
guelaoismael <guelaoismael@gmail.com>
2024-11-11 21:42:02 +0100

    add list_books and library_detail view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views.generic.detail import DetailView
 
 from .models import Library
 from .models import Book
@@ -11,7 +11,7 @@ def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books' : books})
 
-class LibraryDetailsView(generic.ListView):
+class LibraryDetailsView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
