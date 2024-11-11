9cb8f1540d7560830d1593b4d442d5b82d6f1f34
guelaoismael <guelaoismael@gmail.com>
2024-11-11 17:54:41 +0100

    add new app relationship_app

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -4,14 +4,26 @@ from django.db import models
 class Author(models.Model):
   name = models.CharField(max_length=120)
 
+  def __str__(self):
+    return f"{self.name}"
+
 class Book(models.Model):
   title = models.CharField(max_length=120)
   author = models.ForeignKey(Author, on_delete= models.CASCADE, related_name="books")
+
+  def __str__(self):
+    return f"{self.title} - {self.author}"
 
 class Library(models.Model):
   name = models.CharField(max_length=120)
   books = models.ManyToManyField(Book)
 
+  def __str__(self):
+    return f"{self.name} - {self.books}"
+
 class Librarian(models.Model):
   name = models.CharField(max_length=120)
   library = models.OneToOneField(Library, on_delete= models.CASCADE)
+  
+  def __str__(self):
+    return f"{self.name} - {self.library}"
