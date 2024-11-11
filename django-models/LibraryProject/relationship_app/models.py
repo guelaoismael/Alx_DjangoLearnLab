ec8193dc29f3ada841d718dae5332ebf3cf1f1d0
guelaoismael <guelaoismael@gmail.com>
2024-11-11 17:56:13 +0100

    add new app relationship_app

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -5,7 +5,7 @@ class Author(models.Model):
   name = models.CharField(max_length=120)
 
   def __str__(self):
-    return f"{self.name}"
+    return self.name
 
 class Book(models.Model):
   title = models.CharField(max_length=120)
