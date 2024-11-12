a4c0e50fc217e25a75059aeb5cd6cc45a164b551
guelaoismael <guelaoismael@gmail.com>
2024-11-12 10:57:44 +0100

    add RegistrationView class

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import  reverse_lazy
 
 from .models import Library
 from .models import Book
@@ -11,9 +14,15 @@ def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books' : books})
 
-class LibraryDetailsView(DetailView):
+class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
     def get_queryset(self):
         return Library.objects.all()
+    
+
+class RegistrationView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'relationship_app/register.html'
