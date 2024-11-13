ad85bfa4c7e876350037d3e4e6bea2fe7492a77d
guelaoismael <guelaoismael@gmail.com>
2024-11-13 17:33:54 +0100

    add views to test permissions

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import permission_required
+from django.http import HttpResponse
+from .models import Post
 
-# Create your views here.
+@permission_required('bookshelf.can_view', raise_exception=True)
+def post_list(request):
+   
+     return HttpResponse("Post list!")
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_post(request):
+    return HttpResponse("Post created!")
+    
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_post(request, article_id):
+    return HttpResponse("Post updated!")
+    
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_post(request, article_id):
+    return HttpResponse("Post deleted!") 
