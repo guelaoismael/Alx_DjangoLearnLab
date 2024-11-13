1a9a166c097d492da600fb0c750de57a2e42851c
guelaoismael <guelaoismael@gmail.com>
2024-11-13 16:02:01 +0100

    Advanced Features and Security TASKS

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class ModelAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+admin.site.register(CustomUser, ModelAdmin)
+
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     list_filters = ('title', 'author', 'publication_year')
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.contrib.auth.models import BaseUserManager, AbstractUser
+
 # Create your models here.
 
 class Book(models.Model):
@@ -9,3 +11,33 @@ class Book(models.Model):
 
   def __str__(self):
     return f"title : {self.title}, author : {self.author}, publication year : {self.publication_year}"
+
+
+class CustomUserManager(BaseUserManager):
+   
+   def create_user(self, username, email, password=None, **extra_fields):
+      
+      email = self.normalize_email(email)
+      user = self.model(username=username, email=email, **extra_fields)
+      user.set_password(password)
+      user.save(using=self._db)
+
+      return user
+   
+
+   def create_superuser(self, username, email, password=None, **extra_fields):
+      
+      extra_fields.setdefault('is_staff', True)
+      extra_fields.setdefault('is_superuser', True)
+
+      return self.create_user(username, email, password, **extra_fields)
+   
+
+# Set up the custom user model
+
+class CustomUser(AbstractUser):
+   
+   date_of_birth = models.DateField()
+   profile_photo = models.ImageField()
+
+   objects = CustomUserManager()
