8e4b87c137733935fb3e390cca7d65f0f7f28430
guelaoismael <guelaoismael@gmail.com>
2024-12-03 12:28:00 +0100

    Initial Setup and Project Configuration for a Django Blog

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,7 +1,6 @@
 from django.db import models
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 
-User = get_user_model()
 
 # Create your models here.
 class Post (models.Model):
