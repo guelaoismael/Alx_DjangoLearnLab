b8e61222ecafbf1f5242ae09f14b80c912afd767
guelaoismael <guelaoismael@gmail.com>
2024-12-03 16:00:39 +0100

    Implementing the Blog's User Authentication System

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,19 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from .forms import CreationForm, UserProfileForm
 
 # Create your views here.
 
-class RegistrationView(CreateView):
-  
-  form_class = UserCreationForm
-  success_url = reverse_lazy("login")
-  template_name = "blog/signup.html"
+def registration_view(request):
+    if request.method == 'POST':
+        form = CreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = CreationForm()
+    return render(request, 'blog/signup.html', {'form': form})
 
 
 @login_required
 def profile_view(request):
-    # This view can only be accessed by authenticated users
-    return render(request, 'blog/profile.html')
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = UserProfileForm(instance=request.user)
+    return render(request, 'blog/profile.html', {'form': form})
