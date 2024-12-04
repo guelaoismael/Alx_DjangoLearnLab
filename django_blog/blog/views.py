0451d19bc709cd426893dc2f0030b1ea082732c8
guelaoismael <guelaoismael@gmail.com>
2024-12-04 17:14:44 +0100

    Adding Comment Functionality to Blog Posts

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, redirect
-from .forms import PostForm, RegisterForm, UserProfileForm
+from django.shortcuts import get_object_or_404, render, redirect
+from .forms import PostForm, RegisterForm, UserProfileForm, CommentForm
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse, reverse_lazy
 from django.views import generic
-from .models import Post
+from .models import Post, Comment
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 # Create your views here.
 
@@ -69,3 +69,61 @@ class DeleteView(generic.DeleteView, LoginRequiredMixin, UserPassesTestMixin):
     success_url = reverse_lazy('posts')
 
 
+# class AddCommentView(generic.View):
+     
+#     def get(self, request, post_id):
+#         post = get_object_or_404(Post, id=post_id)
+#         form = CommentForm()
+#         return render(request, 'blog/comments/add_comment.html', {'form': form, 'post': post})
+
+#     def post(self, request, post_id):
+#         post = get_object_or_404(Post, id=post_id)
+#         form = CommentForm(request.POST)
+
+#         if form.is_valid():
+#             form.instance.author = request.user
+#             form.instance.post = post
+#             form.save()
+#             return redirect('detail-post', pk=post.id)
+#         return render(request, 'blog/comments/add_comment.html', {'form': form, 'post': post})
+
+
+   
+
+class CommentListView(generic.ListView):
+    model = Comment
+    template_name = "TEMPLATE_NAME"
+
+
+class AddCommentView(generic.CreateView, LoginRequiredMixin):
+   
+   model = Comment
+   fields = ['content']
+   template_name="blog/comments/add_comment.html"
+
+   def get_success_url(self):
+      return reverse_lazy('detail-post', kwargs={'pk': self.kwargs['post_id']})
+   
+   def form_valid(self, form):
+       form.instance.author = self.request.user  
+       form.instance.post = get_object_or_404(Post, pk=self.kwargs['post_id']) 
+       return super().form_valid(form)
+   
+class EditCommentView(generic.UpdateView, LoginRequiredMixin, UserPassesTestMixin):
+   
+   model = Comment
+   fields = ['content']
+   template_name="blog/comments/edit_comment.html"
+
+   def get_success_url(self):
+      comment = self.get_object()
+      return reverse_lazy('detail-post', kwargs={'pk': comment.post.pk})
+   
+
+class CommentDeleteView(generic.DeleteView, LoginRequiredMixin, UserPassesTestMixin):
+    model = Comment
+    template_name = "blog/comments/delete_comment.html"
+    
+    def get_success_url(self):
+      comment = self.get_object()
+      return reverse_lazy('detail-post', kwargs={'pk': comment.post.pk})
