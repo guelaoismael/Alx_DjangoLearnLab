03d624ecd9693afc7afa628547b2438c0e362933
guelaoismael <guelaoismael@gmail.com>
2024-12-14 11:00:35 +0100

    Deploying the Django REST API to Production

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -100,12 +100,23 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'root',
+        'HOST': 'localhost',
+        'PASSWORD': 'password',
+        'PORT': '3306'
     }
 }
+
 
 
 # Password validation
